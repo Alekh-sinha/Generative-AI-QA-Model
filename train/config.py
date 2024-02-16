c0a2ef53a4d13de8804f265bce2eace3eafe5745
Alekh-sinha <alekh.sinha09@gmail.com>
2024-02-16 12:13:12 +0530

    final code

--- train/config.py
+++ train/config.py
@@ -7,14 +7,13 @@ Created on Sun Jan 21 02:58:23 2024
 
 class cfg:
     def __init__(self):
-        self.path='latest_file'
+        self.path='latest_file' #folder where latest file whose embedding are not yet generated are kept
         self.tokens_per_chunk=256
         self.collection='upsc_2024'
-        self.old_file='gs://upsc_2024/old_file'
-        self.chroma='gs://upsc_2024/chromadb'
+        self.old_file='gs://upsc_2024/old_file' # folder location where files after embedding generation are moved
+        self.chroma='gs://upsc_2024/chromadb'#Folder where all chroma collections are stored
         self.model='mistralai/Mistral-7B-Instruct-v0.1'
-        self.bucket='upsc_2024'
-        self.data='data.json'
+        self.data='data.json'#Input data file for training
         self.max_length=512
         self.batch_size=1
         self.epochs=1
@@ -23,5 +22,5 @@ class cfg:
         self.GCS_STAGING = "gs://upsc_2024/pipeline_root/"
         self.MACHINE_TYPE="n1-highmem-8"
         self.REPLICA_COUNT=1
-        self.TRAIN_IMAGE_URI='gcr.io/qualified-abode-411820/pytorch_training_upsc_2024'
-        self.PROJECT_ID='qualified-abode-411820'
+        self.TRAIN_IMAGE_URI='gcr.io/XXXXX/pytorch_training_upsc_2024'
+        self.PROJECT_ID='XXXXX'
